b8a74155bdda9daa67a8f858d26e662d444a168c
suecodingm <susana.mendoza.neira@gmail.com>
2025-08-04 10:01:09 -0500

    Update SpeakHello.js

--- module_4_solution/SpeakHello.js
+++ module_4_solution/SpeakHello.js
@@ -28,7 +28,7 @@
 (function   (window) {
   var helloSpeaker = {};
   var speakWord = "Hello";
-  helloSpeaker.speak = function () {
+  helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
 
