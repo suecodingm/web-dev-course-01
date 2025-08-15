3153139e0ad0ac5af5fee1de4130c5fe0ee6039a
suecodingm <susana.mendoza.neira@gmail.com>
2025-08-15 12:23:31 -0500

    Update script.js

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -114,8 +114,8 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      //
-       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", 'chosenCategoryShortName.short_name');
+      //  portionValue = "(" + portionValue + ")";
+       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'"+chosenCategoryShortName.short_name+"'");
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
@@ -342,6 +342,7 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
+
 
 
 
